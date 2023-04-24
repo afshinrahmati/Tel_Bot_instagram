0a07d5d3bf51d37ee69c8b5d5c7db8090db51b1d
a.rahmati <a.rahmati@kianiranian.com>
2023-04-24 16:05:39 +0330

    test

---
+++ src/telegram/telegram.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, Logger } from '@nestjs/common';
+import Tel = require('node-telegram-bot-api');
+const TELEGRAM_TOKEN = '6025493105:AAH8CYpkbEfTZ-uBXN3HpGgY_xoRIdFFUKc';
+@Injectable()
+export class TelegramService {
+  private bot: any;
+  private logger = new Logger(TelegramService.name);
+
+  constructor() {
+    this.bot = new Tel(TELEGRAM_TOKEN, { polling: true });
+    this.bot.on('message', this.onReceiveMessage);
+  }
+  onReceiveMessage = (msg: any) => {
+    this.logger.debug(msg);
+  };
+}
